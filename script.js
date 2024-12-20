9580bb887b4b1609c802d3b65ec87c035136bf41
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-12-20 14:35:24 -0500

    Create 'display books' function

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ function addBookToLibrary(title, author, pages, haveRead) {
     let book = new Book(title, author, pages, haveRead);
     myLibrary.push(book);
 }
+
+let cardContainer = document.querySelector(".card-container");
+
+function displayBooks(library) {
+    for (const book of library) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        cardContainer.appendChild(card);
+        const title = document.createElement("div");
+        title.classList.add("title");
+        title.textContent = `${book.title}`;
+        card.appendChild(title);
+        const author = document.createElement("p");
+        author.textContent = `by ${book.author}`;
+        card.appendChild(author);
+        const pages = document.createElement("p");
+        pages.textContent = `${book.pages} pages`
+        card.appendChild(pages);
+        const haveRead = document.createElement("p");
+        haveRead.textContent = `${book.haveRead}`;
+        card.appendChild(haveRead);
+    }
+}
